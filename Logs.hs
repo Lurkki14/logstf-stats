a689313389f7c0458909b5a1687b289c6a8747e9
Jussi Kuokkanen <jussi.kuokkanen@protonmail.com>
2020-11-22 13:15:52 +0200

    add option parsing and clean up imports

--- Logs.hs
+++ Logs.hs
@@ -7,20 +7,20 @@ import GHC.Generics
 import Network.Http.Client
 import Control.Concurrent (threadDelay)
 import Control.Monad (join)
-import Data.Aeson
-import Data.Aeson.Types
-import qualified Data.Binary as B
-import Data.ByteString
+import Data.Aeson hiding (Options)
+import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
-import Data.HashMap.Lazy
-import Data.Text
+import qualified Data.HashMap.Lazy as HML
+import Data.Text ()
 import OpenSSL
-
-testPath = "/json/2752512"
+import Options.Generic
 
 addr = "logs.tf"
 
---steamID = "[U:1:37776901]"
+data Options = Options {
+  steamID64 :: Int
+, steamID3 :: Text
+} deriving (Generic, ParseRecord, Show)
 
 data MatchInfo = MatchInfo {
   length :: Int
@@ -41,12 +41,11 @@ data LogQuery = LogQuery {
 } deriving (Show, Generic, FromJSON)
 
 data PlayerLog = PlayerLog {
-  --n :: Text, 
   id :: Int
 } deriving (Show, Generic, FromJSON)
 
 instance {-# OVERLAPPING #-} FromJSON [PlayerStats] where
-  parseJSON x = parseJSON x >>= mapM parseEntry . toList where
+  parseJSON x = parseJSON x >>= mapM parseEntry . HML.toList where
     parseEntry (t, v) = withObject "[PlayerStats]" (\o ->
       PlayerStats t <$> o .: "kills" <*> o .: "deaths") v
       --v is the JSON value containing the player objects with SteamID fields
@@ -68,22 +67,26 @@ matchInfo conn id = do
       http GET $ "/json/" <> id
       setAccept "application/json"
   threadDelay 100000 --Delay since logs.tf doesn't seem to like the frequent requests
-  --print $ show id
   sendRequest conn req emptyBody
   receiveResponse conn jsonHandler :: IO MatchInfo
 
+toByteString :: Show a => a -> ByteString
+toByteString x = BS.pack $ show x
+
 main = withOpenSSL $ do
+  opts <- getRecord "logstf-stats" :: IO Options
+
   ctx <- baselineContextSSL
   conn <- openConnectionSSL ctx addr 443
   
-  logQuery <- playerLogs conn "76561198078484073"
+  logQuery <- playerLogs conn $ toByteString $ steamID64 opts
   let ids = fmap Main.id $ logs $ logQuery 
-  let binIDs = fmap (BS.pack . show) ids
+  let binIDs = fmap toByteString ids
   let matchInfosM = fmap (matchInfo conn) binIDs
-  let logs10 = Prelude.take 20 matchInfosM
+  let logs10 = Prelude.take 30 matchInfosM
   stats <- sequence logs10
   let playerStats = join $ fmap players stats
-  let onePlayerStats = Prelude.filter (\ps -> steamID ps == "[U:1:118218345]") playerStats 
+  let onePlayerStats = filter ((steamID3 opts ==) . steamID) playerStats 
   let kills' = fmap kills onePlayerStats
   print $ (show $ sum kills') <> " kills in 20 matches"
 
--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ let
 
   nixPackages = [
     (haskellPackages.ghcWithPackages (pkgs: with pkgs; [
-      http-streams
+      http-streams optparse-generic
     ]))
   ];
 in
